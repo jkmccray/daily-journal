3697f4c90b9ed001dc01e3e336d9c3eac5c240e4
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-11 15:57:48 -0500

    Worked on Daily Journal Part 2

---
+++ src/scripts/journal.js
@@ -0,0 +1,26 @@
+let allJournalEntries = [];
+
+const journalEntry1 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+const journalEntry2 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+const journalEntry3 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+allJournalEntries.push(journalEntry1, journalEntry2, journalEntry3);
+
+console.log(allJournalEntries);
