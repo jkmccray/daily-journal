684c70726d77d5c246e889d742d25668cb38ee5d
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-08-12 10:08:50 -0500

    added entry for friday and yesterday. added non-functional edit button

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -5,6 +5,7 @@ const entryComponent = {
       <header class="entryHeader">
       <div class="date-and-delete">
       <h1 class="date">Date: ${journalEntryObj.date}</h1>
+      <button class="edit-btn" id="editBtn--${journalEntryObj.id}">Edit</button>
       <button class="delete-btn" id="deleteBtn--${journalEntryObj.id}">Delete</button>
       </div>
       <h2 class="concepts">${journalEntryObj.concepts}</h2>
